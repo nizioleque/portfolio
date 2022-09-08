f2734e366be6ce4a25532e6c61c5612a5c73ba42
nizioleque <nizioleque@gmail.com>
2022-09-09 00:10:10 +0200

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import '../src/index.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
