40b58e8505e2dcb6edd27916b625d38770977630
Norbert Niziołek <nizioleque@gmail.com>
2023-02-12 13:57:43 +0100

    Add offscreen canvas to optimize GPU usage

--- src/components/CanvasBackground.tsx
+++ src/components/CanvasBackground.tsx
@@ -13,35 +13,64 @@ function getHue() {
 
 function CanvasBackground() {
   const ctx = useRef<CanvasRenderingContext2D | null>(null);
+  const offscreenCtx = useRef<CanvasRenderingContext2D | null>(null);
 
   const handleMouseMove = useCallback((event: MouseEvent) => {
-    console.log('a');
-    if (!ctx.current) return;
+    if (!ctx.current || !offscreenCtx.current) return;
 
-    ctx.current.fillStyle = `hsl(${getHue()},100%,${DRAW_LIGHTNESS}%,${DRAW_OPACITY})`;
-    ctx.current.beginPath();
-    ctx.current.arc(event.clientX, event.clientY, DRAW_RADIUS, 0, 2 * Math.PI);
-    ctx.current.fill();
-  }, []);
+    offscreenCtx.current.fillStyle = `hsl(${getHue()},100%,${DRAW_LIGHTNESS}%,${DRAW_OPACITY})`;
+    offscreenCtx.current.beginPath();
+    offscreenCtx.current.arc(
+      event.clientX,
+      event.clientY,
+      DRAW_RADIUS,
+      0,
+      2 * Math.PI
+    );
+    offscreenCtx.current.fill();
 
-  const handleResize = useCallback(() => {
-    if (!ctx.current) return;
-    console.log(document.body.clientHeight, document.body.clientWidth);
-    const imageData = ctx.current.getImageData(
+    ctx.current.clearRect(
       0,
       0,
       ctx.current.canvas.width,
       ctx.current.canvas.height
     );
+    ctx.current.drawImage(offscreenCtx.current.canvas, 0, 0);
+  }, []);
+
+  const handleResize = useCallback(() => {
+    if (!ctx.current || !offscreenCtx.current) return;
+
+    console.log(document.body.clientHeight, document.body.clientWidth);
+    const imageData = offscreenCtx.current.getImageData(
+      0,
+      0,
+      ctx.current.canvas.width,
+      ctx.current.canvas.height
+    );
+
+    offscreenCtx.current.canvas.width = document.body.clientWidth;
+    offscreenCtx.current.canvas.height = document.body.clientHeight;
+
     ctx.current.canvas.width = document.body.clientWidth;
     ctx.current.canvas.height = document.body.clientHeight;
-    ctx.current.putImageData(imageData, 0, 0);
+    ctx.current.filter = `blur(${BLUR_RADIUS}px)`;
+
+    offscreenCtx.current.putImageData(imageData, 0, 0);
+    ctx.current.drawImage(offscreenCtx.current.canvas, 0, 0);
   }, []);
 
   useEffect(() => {
     if (!ctx.current) return;
+
+    const offscreenCanvas = document.createElement('canvas');
+    offscreenCanvas.width = document.body.clientWidth;
+    offscreenCanvas.height = document.body.clientHeight;
+    offscreenCtx.current = offscreenCanvas.getContext('2d');
+
     ctx.current.canvas.width = document.body.clientWidth;
     ctx.current.canvas.height = document.body.clientHeight;
+    ctx.current.filter = `blur(${BLUR_RADIUS}px)`;
 
     document.body.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('resize', handleResize);
@@ -57,9 +86,6 @@ function CanvasBackground() {
       ref={(node) => {
         if (!node) return;
         ctx.current = node?.getContext('2d');
-      }}
-      style={{
-        filter: `blur(${BLUR_RADIUS}px)`,
       }}
     />
   );
