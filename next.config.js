de49cfd715ca5d90276d95dea6598d5744160988
Norbert Niziołek <nizioleque@gmail.com>
2023-03-06 21:28:13 +0100

    Add rewrite for project pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  rewrites: async () => [
+    {
+      source: '/:path*',
+      destination: '/projects/:path*',
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/projects/mouse-pinch-to-zoom.tsx
@@ -0,0 +1,5 @@
+function MousePinchToZoom() {
+  return <div>MousePinchToZoom</div>;
+}
+
+export default MousePinchToZoom;
