c2c9377464704d6f123aca76ef7077830885ee9e
Norbert Niziołek <nizioleque@gmail.com>
2024-01-09 15:19:11 +0100

    Hide content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import '../src/index.css';
-import type { AppProps } from 'next/app';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from '../src/theme';
+import { CssBaseline, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+import "../src/index.css";
+import { theme } from "../src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
+      <Typography variant="h1">Hello world</Typography>
     </ThemeProvider>
   );
 }
