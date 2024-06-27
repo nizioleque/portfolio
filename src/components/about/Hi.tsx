5d9d3e6639ba86e6fe28f64e35fefebcb76ebed4
Norbert Niziołek <nizioleque@gmail.com>
2024-06-27 21:48:46 +0200

    Add avatar (funny)

--- src/components/about/Hi.tsx
+++ src/components/about/Hi.tsx
@@ -1,6 +1,7 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import Image from "next/image";
+import avatarFunny from "../../assets/avatar-funny.jpg";
 import avatar from "../../assets/avatar.jpg";
 import HomePageChild from "../Layout/HomePageChild";
 import ChatBubble from "./ChatBubble";
@@ -13,6 +14,16 @@ function Hi() {
       <Stack direction="row" alignItems="center" gap={4}>
         <Avatar sx={{ height: AvatarSize, width: AvatarSize }}>
           <Image alt="Avatar" src={avatar} fill />
+          <Box
+            component={Image}
+            src={avatarFunny}
+            fill
+            alt=""
+            sx={{
+              opacity: 0,
+              "&:hover": { opacity: 1 },
+            }}
+          />
         </Avatar>
         <ChatBubble>Hi!</ChatBubble>
       </Stack>
