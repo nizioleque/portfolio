c2f3c18a0d6c686f738091cd374870030b990ae7
Norbert Niziołek <norbert@niziolek.dev>
2025-04-14 04:33:49 +0200

    Change 700 color to 800

--- src/components/cv/MainSection.tsx
+++ src/components/cv/MainSection.tsx
@@ -10,11 +10,11 @@ function MainSection({ title, subtitle, children }: MainSectionProps) {
   return (
     <div className="flex flex-col gap-3">
       <div className="flex items-center gap-2">
-        <h2 className="text-cv-700 text-xl font-semibold uppercase">{title}</h2>
+        <h2 className="text-cv-800 text-xl font-semibold uppercase">{title}</h2>
         {subtitle && (
-          <div className="text-cv-700 text-sm italic underline">{subtitle}</div>
+          <div className="text-cv-800 text-sm italic underline">{subtitle}</div>
         )}
-        <div className="border-cv-700/50 ms-2 -me-6 flex flex-1 items-center gap-2 border-t-2" />
+        <div className="border-cv-800/50 ms-2 -me-6 flex flex-1 items-center gap-2 rounded-l-full border-t-2" />
       </div>
       <div className="grid grid-cols-2 gap-x-6 gap-y-6">{children}</div>
     </div>
