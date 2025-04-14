23d8d626751d6c72015d167e3c7bb0060a4677e2
Norbert Niziołek <norbert@niziolek.dev>
2025-04-14 04:34:41 +0200

    Fix section subtitle alignment

--- src/components/cv/MainSection.tsx
+++ src/components/cv/MainSection.tsx
@@ -9,12 +9,18 @@ interface MainSectionProps {
 function MainSection({ title, subtitle, children }: MainSectionProps) {
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex items-center gap-2">
-        <h2 className="text-cv-800 text-xl font-semibold uppercase">{title}</h2>
-        {subtitle && (
-          <div className="text-cv-800 text-sm italic underline">{subtitle}</div>
-        )}
-        <div className="border-cv-800/50 ms-2 -me-6 flex flex-1 items-center gap-2 rounded-l-full border-t-2" />
+      <div className="flex items-center gap-4">
+        <div className="flex items-baseline gap-2">
+          <h2 className="text-cv-800 text-xl font-semibold uppercase">
+            {title}
+          </h2>
+          {subtitle && (
+            <div className="text-cv-800 text-sm italic underline">
+              {subtitle}
+            </div>
+          )}
+        </div>
+        <div className="border-cv-800/50 -me-6 flex flex-1 items-center gap-2 rounded-l-full border-t-2" />
       </div>
       <div className="grid grid-cols-2 gap-x-6 gap-y-6">{children}</div>
     </div>
